b0b648481317713b14064f3e023ff6dab99b020a
BASS <PAWAT_BEST@WINDOWSLIVE.COM>
2022-01-21 13:17:02 +0700

    1st

---
+++ README.md
@@ -0,0 +1,2 @@
+step 1 : npm i
+step 2 : api.rest เป็นตัวยิง api ดาวน์โหลด extension REST Client จาก visual code มาก็ใช้ไฟล์ api.rest ได้เลย
---
+++ index.js
@@ -0,0 +1,134 @@
+const express = require('express')
+const app = express()
+const sha256 = require('crypto-js/sha256')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+class Block {
+    constructor(
+        index,
+        timestamp,
+        transaction,
+        precedingHash = ''
+    ) {
+        this.index = index
+        this.timestamp = timestamp
+        this.transaction = transaction
+        this.precedingHash = precedingHash
+        this.hash = this.computeHash()
+    }
+    computeHash() {
+        return sha256(
+            this.index +
+            this.precedingHash +
+            this.timestamp +
+            JSON.stringify(this.transaction)
+        ).toString()
+    }
+}
+
+class BlockChain {
+    constructor() {
+        this.id = ''
+        this.name = ''
+        this.blockchain = ''
+        this.difficulty = ''
+    }
+
+    create(id, name, genesis) {
+        this.id = id
+        this.name = name
+        this.blockchain = [this.startGenesisblock(genesis)]
+        this.difficulty = 4
+    }
+    startGenesisblock(genesis) {
+        return new Block(
+            0,
+            genesis.date,
+            genesis.transaction,
+            "0"
+        )
+    }
+    obtainLatestBlock() {
+        return this.blockchain[this.blockchain.length - 1]
+    }
+    addNewBlock(newBlock) {
+        newBlock.precedingHash = this.obtainLatestBlock().hash
+        newBlock.hash = newBlock.computeHash()
+        this.blockchain.push(newBlock)
+    }
+    checkChainValidity() {
+        for (let i = 1; i < this.blockchain.length; i++) {
+            const currentBlock = this.blockchain[i]
+            const precedingBlock = this.blockchain[i - 1]
+
+            if (currentBlock.hash !== currentBlock.computeHash()) {
+                return false
+            }
+            if (currentBlock.predcedingHash !== precedingBlock.hash()) {
+                return false
+            }
+        }
+        return true
+    }
+}
+
+const GlobalChain = new BlockChain()
+
+class BassCoin {
+    constructor() {
+        this.chain = []
+    }
+    validateNewChain = (req, res, next) => {
+        if (req.body) {
+            if (req.body.id &&
+                req.body.name &&
+                req.body.genesis &&
+                req.body.genesis.date &&
+                req.body.genesis.transaction
+            ) {
+                next()
+            } else {
+                res.status(400).json({ message: 'requuest format invalid' })
+            }
+        } else {
+            res.status(400).json({ message: 'request format invalid' })
+        }
+    }
+    createNewChain = (req, res) => {
+        const block = GlobalChain.create(
+            req.body.id,
+            req.body.name,
+            req.body.genesis
+        )
+        res.status(200).json({ message: 'chain created', data: block })
+    }
+
+    appendNewChild = (req, res) => {
+        const block = new Block(
+            GlobalChain.blockchain.length,
+            req.body.timestamp,
+            req.body.transaction
+        )
+        GlobalChain.addNewBlock(block)
+        res.status(200).json({ message: 'Block added' })
+    }
+    getChain = (req, res) => {
+        res.status(200).json({ chain: GlobalChain })
+    }
+}
+
+const Controller = new BassCoin()
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'pass' })
+})
+
+app.post('/api/blockchain', Controller.validateNewChain, Controller.createNewChain)
+app.get('/api/blockchain', Controller.getChain)
+app.post('/api/blockchain/append', Controller.appendNewChild)
+
+app.listen(3000, () => {
+    console.log('blockchain running on 3000')
+})
